16e3d9df4a30761489ec4d99626e42ceb00f365a
yiyun <128479933+wonder0091@users.noreply.github.com>
2024-07-02 22:37:11 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -15,7 +15,7 @@ const CONFIG = {
   NEXT_RIGHT_BAR: true, // 是否显示右侧栏
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
-  NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
+  NEXT_RIGHT_TAG_LIST: false, // 右侧边栏标签分类列表
   NEXT_RIGHT_AD: false, // 右侧广告
 
   // 菜单
@@ -31,7 +31,7 @@ const CONFIG = {
   NEXT_WIDGET_TOC: true, // 移动端显示悬浮目录
 
   NEXT_ARTICLE_RELATE_POSTS: true, // 相关文章推荐
-  NEXT_ARTICLE_COPYRIGHT: false, // 文章版权声明
+  NEXT_ARTICLE_COPYRIGHT: true, // 文章版权声明
   NEXT_ARTICLE_INFO: true // 显示文章信息
 
 }
