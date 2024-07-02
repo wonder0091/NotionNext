3edf083695abde823568c25983d434ee64464228
yiyun <128479933+wonder0091@users.noreply.github.com>
2024-07-02 22:23:12 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -20,8 +20,8 @@ const CONFIG = {
 
   // 菜单
   NEXT_MENU_HOME: true, // 显示首页
-  NEXT_MENU_CATEGORY: true, // 显示分类
-  NEXT_MENU_TAG: true, // 显示标签
+  NEXT_MENU_CATEGORY: false, // 显示分类
+  NEXT_MENU_TAG: false, // 显示标签
   NEXT_MENU_ARCHIVE: true, // 显示归档
   NEXT_MENU_SEARCH: true, // 显示搜索
 
