bba29519865df59f2daeeba4f550fa50afe8dd82
yy <128479933+wonder0091@users.noreply.github.com>
2024-09-18 18:57:36 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  MATERY_HOME_BANNER_ENABLE: true,
+  MATERY_HOME_BANNER_ENABLE: false,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   MATERY_HOME_BANNER_GREETINGS: ['欢迎来到我的博客🎉'], // 首页大图标语文字
 
