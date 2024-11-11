3f1b40730af245db289d42a6f3fad08d075c44d2
yy <128479933+wonder0091@users.noreply.github.com>
2024-11-11 20:49:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -238,7 +238,8 @@ const BLOG = {
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
         process.env.NEXT_PUBLIC_WIDGET_PET_LINK ||
-        'https://imuncle.github.io/live2d/model/murakumo/index.json', 
+        'https://imuncle.github.io/live2d/model/rem/model.json',
+  //丛云 https://imuncle.github.io/live2d/model/murakumo/index.json
   //haru02 https://cdn.jsdelivr.net/npm/live2d-widget-model-haru02@1.0.2/assets/haru02.model.json
   // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
