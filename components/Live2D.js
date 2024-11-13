c269da25d124948ae21655b60a8d3d9135d3fc50
yy <128479933+wonder0091@users.noreply.github.com>
2024-11-13 13:47:36 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -13,22 +13,21 @@ export default function Live2D() {
   const showPet = JSON.parse(siteConfig('WIDGET_PET'))
   const petLink = siteConfig('WIDGET_PET_LINK')
 
-  useEffect(() => {
-    if (showPet && !isMobile()) {
-      Promise.all([
-        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
-      ]).then((e) => {
-        if (typeof window?.loadlive2d !== 'undefined') {
-          // https://github.com/xiazeyu/live2d-widget-models
-          try {
-            loadlive2d('live2d', petLink)
-          } catch (error) {
-            console.error('读取PET模型', error)
-          }
+useEffect(() => {
+  if (showPet && !isMobile()) { // 确保移动端不加载模型
+    Promise.all([
+      loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
+    ]).then((e) => {
+      if (typeof window?.loadlive2d !== 'undefined') {
+        try {
+          loadlive2d('live2d', petLink)
+        } catch (error) {
+          console.error('读取PET模型', error)
         }
-      })
-    }
-  }, [theme])
+      }
+    })
+  }
+}, [theme])
 
   function handleClick() {
     if (JSON.parse(siteConfig('WIDGET_PET_SWITCH_THEME'))) {
